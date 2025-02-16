282c3751210e313b899b19e185cb82d667454d9d
CarlosGajard <cgajagon@gmail.com>
2025-02-16 18:56:37 -0500

    Refactor login logic to use secrets for authentication and resolve merge conflicts in JSON index store files

--- pages/login.py
+++ pages/login.py
@@ -5,14 +5,13 @@ import streamlit as st
 
 
 st.title("Welcome to Diamond Corp")
-
 st.info("Please log in to continue.")
 
 username = st.text_input("Username")
 password = st.text_input("Password", type="password")
 
 if st.button("Log in", type="primary"):
-    if username == "test" and password == "test":
+    if username == st.secrets.admin.user and password == st.secrets.admin.password:
         st.session_state.logged_in = True
         st.success("Logged in successfully!")
         sleep(0.5)
